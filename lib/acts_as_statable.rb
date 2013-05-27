052bb7ab6c9a4a6a5233c55df52504de12f61192
Ghislain de Juvigny <gdjuvigny@gmail.com>
2013-05-27 19:35:54 +0200

    test mongoid

--- lib/acts_as_statable.rb
+++ lib/acts_as_statable.rb
@@ -1,88 +1,10 @@
 require "acts_as_statable/version"
+require "acts_as_statable/statable"
 require "chartkick/engine"
 require "chartkick/helper"
 
 module ActsAsStatable
   def self.included(base)
-    base.extend(ClassMethods)
-  end
-
-  module ClassMethods
-    def group_by_minute(field)
-      map = %Q{
-        function() {
-          var created_at = new Date(this.#{field}.getFullYear(),
-                                            this.#{field}.getMonth(), 
-                                            this.#{field}.getDate(), 
-                                            this.#{field}.getHours(), 
-                                            this.#{field}.getMinutes()
-                                          );
-          emit(created_at, {count: 1});
-        }
-      }
-
-      self.group_by_time(map)
-    end
-
-    def group_by_hour(field)
-      map = %Q{
-        function() {
-          var created_at = new Date(this.#{field}.getFullYear(),
-                                            this.#{field}.getMonth(), 
-                                            this.#{field}.getDate(), 
-                                            this.#{field}.getHours()
-                                          );
-          emit(created_at, {count: 1});
-        }
-      }
-
-      self.group_by_time(map)
-    end
-
-    def group_by_date(field)
-      map = %Q{
-        function() {
-          var created_at = new Date(this.#{field}.getFullYear(),
-                                            this.#{field}.getMonth(), 
-                                            this.#{field}.getDate()
-                                          );
-          emit(created_at, {count: 1});
-        }
-      }
-
-      self.group_by_time(map)
-    end
-
-    def group_by_month(field)
-      map = %Q{
-        function() {
-          var created_at = new Date(this.#{field}.getFullYear(),
-                                            this.#{field}.getMonth()
-                                          );
-          emit(created_at, {count: 1});
-        }
-      }
-
-      self.group_by_time(map)
-    end
-
-    def group_by_time(map)
-
-      reduce = %Q{
-        function(key, values) { 
-           var total = 0;
-           for(var i = 0; i < values.length; i++) { total += values[i].count; }
-           return {count: total};
-        }
-      }
-
-      output = []
-
-      self.all.map_reduce(map, reduce).out(inline: true).each do |item|
-        output << [item["_id"], item["value"]["count"]]
-      end
-
-      output
-    end
+    base.extend(ActsAsStatable::Mongoid)
   end
 end
