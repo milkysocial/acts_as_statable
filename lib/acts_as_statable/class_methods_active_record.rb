93b6d42cabf753efe680ab7d127e887895a527f3
Ghislain de Juvigny <gdjuvigny@gmail.com>
2013-06-27 18:01:23 +0200

    fix

---
+++ lib/acts_as_statable/class_methods_active_record.rb
@@ -0,0 +1,16 @@
+module ClassMethodsActiveRecord
+  def group_by_minute(field)
+  end
+
+  def group_by_hour(field)
+  end
+
+  def group_by_date(field)
+  end
+
+  def group_by_month(field)
+  end
+
+  def group_by_time(map)
+  end
+end
--- lib/acts_as_statable/helper.rb
+++ lib/acts_as_statable/helper.rb
@@ -3,5 +3,14 @@ module ActsAsStatable
     def line_chart_model(model, options = {})
       line_chart(model.group_by_date(:created_at))
     end
+
+    def chart_box(model, title)
+      %Q{
+        <div class="well">
+          <h3>#{title} : #{model.all.count}</h3>
+          #{line_chart_model model}
+        </div>
+      }
+    end
   end
 end
